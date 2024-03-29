56dbef1b8e18b35672d54a39813e5a7cc0187583
Shubham <shubhampharande8@gmail.com>
2024-03-29 15:05:44 -0400

    Social Media Frontend

---
+++ SocialMedia-Frontend/src/Data/PostsData.js
@@ -0,0 +1,36 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+
+export const PostsData = [
+    {
+        img: NaN,
+        name: 'Tzuyu',
+        desc: "SOme big misinformation post here ygthajdifshdufshisbjauifosh bjfiofsdhbjhgaktyfrcjhluygftuyftdrhtxfhgtyughj cuihjviuyhjcfyutgyughjciyuhkjvuikjv guijvuiyfhjcyiufhjcvuikhvjbubkjvbukjb asdagysiuldhasd",
+        likes: 5,
+        liked: false
+    },
+    {
+        img: postPic1,
+        name: 'Tzuyu',
+        desc: "Happy New Year all friends! #2023",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Maryam',
+        desc: "Party time :)",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Salena Gomez",
+        desc: "At Archery Festival",
+        likes: 800,
+        liked: false
+    }
+]
