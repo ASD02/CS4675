3ee6df69c52375ee0b52853e79857989d06753fa
Shubham <shubhampharande8@gmail.com>
2024-04-17 19:12:00 -0400

    Adding the API and fixing DB calls

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -1,25 +1,22 @@
 
-def heuristic(postId, user_votes, user_trust):
+def heuristic(nlp_model_prediction: int, user_votes: dict, user_trust: dict) -> int:
 
     # Placeholders for the data  
     '''
     user_votes = {
-        'trusted_user_1': 1,
-        'trusted_user_2': -1,
-        'untrusted_user_1': 1,
-        'untrusted_user_2': 1,
+        'trusted_user_id_1': 1,
+        'trusted_user_id_2': -1,
+        'untrusted_user_id_1': 1,
+        'untrusted_user_id_2': 1,
     }
 
     user_trust = {
-        'trusted_user_1': True,
-        'trusted_user_2': True,
-        'untrusted_user_1': False,
-        'untrusted_user_2': False,
+        'trusted_user_id_1': True,
+        'trusted_user_id_2': True,
+        'untrusted_user_id_1': False,
+        'untrusted_user_id_2': False,
     }
     '''
-
-
-    nlp_model_prediction = True
 
     # Initialize variables
     trusted_votes = []
@@ -37,12 +34,13 @@ def heuristic(postId, user_votes, user_trust):
     untrusted_average = sum(untrusted_votes) / len(untrusted_votes) if untrusted_votes else 0
 
     # weights
-    trusted_weight = 0.35 
+    trusted_weight = 0.35
     untrusted_weight = 0.15
-    nlp_weight = 0.5  
+    nlp_weight = 0.5
 
     # NLP model prediction to a score
-    if nlp_model_prediction:
+    # TODO: Figure out how to score an opinion
+    if nlp_model_prediction > 0:
         nlp_model_score = 1 
     else:
         nlp_model_score = -1
@@ -55,6 +53,6 @@ def heuristic(postId, user_votes, user_trust):
     )
 
     # range 0 - 100
-    normalized_trust_score = (((overall_trust_score + 1) / 2) * 100)
+    normalized_trust_score = int(((overall_trust_score + 1) / 2) * 100)
 
     return normalized_trust_score
--- backend/api_fake_news.py
+++ backend/api_fake_news.py
@@ -1,37 +1,25 @@
-import sys
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
+MODEL_PATH = 'bert-model'
+# Load the model and tokenizer
+model = BertForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=3)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-def classification_news(news, model_path):
-    # Load the model and tokenizer
-    model = BertForSequenceClassification.from_pretrained(model_path, num_labels=3)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model.eval()  # Put the model in evaluation mode
 
-    model.eval()  # Put the model in evaluation mode
 
+def classify_text(news: str) -> int:
     inputs = tokenizer(news, return_tensors="pt", padding=True, truncation=True, max_length=512)
     with torch.no_grad():
         outputs = model(**inputs)
     logits = outputs.logits
     predictions = torch.argmax(logits, dim=-1)
-    print("The outputs are: " + str(logits) + "\n")
+    classification = 0
     if predictions[0] == 2:
-        classification = 'True'
+        classification = 1              # 'True'
     elif predictions[0] == 1:
-        classification = 'Fake'
+        classification = -1              # 'Fake'
     else:
-        classification = 'Opinion'
+        classification = 0              # 'Opinion'
     return classification
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <model_path> <news>")
-        sys.exit(1)
-
-    model_path = sys.argv[1]
-    news = sys.argv[2]
-
-    classification = classification_news(news, model_path)
-    print(f'{classification}')
