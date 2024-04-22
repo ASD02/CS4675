f67096374dc72d40774159866c08a6929af4649a
Shubham <shubhampharande8@gmail.com>
2024-04-22 15:57:19 -0400

    Bugfix: updating running average

--- backend/utils.py
+++ backend/utils.py
@@ -4,7 +4,7 @@ from cluster import *
 def update_post_vote_stats(user_id, user_vote, post_id, post_info):
     if getUser(user_id)['isTrustedUser']:
         votesTrusted = post_info['votesTrusted'] + 1
-        avgTrusted = ((post_info["avgTrsuted"] * post_info['votesTrusted']) + user_vote) / votesTrusted
+        avgTrusted = ((post_info["avgTrusted"] * post_info['votesTrusted']) + user_vote) / votesTrusted
         updatePostTrusted(post_id, votesTrusted, avgTrusted)
     else:
         votesUntrusted = post_info['votesUntrusted'] + 1
